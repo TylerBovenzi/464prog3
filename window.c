22c8d5fdcded5946439f805a0825159cd7d68eb8
Tyler Bovenzi <Tylerbovenzi4427@gmail.com>
2022-05-18 01:19:17 -0700

    more window

--- window.c
+++ window.c
@@ -1,15 +1,16 @@
 #include <stdint-gcc.h>
 #include <malloc.h>
+#include <string.h>
 
 struct frame{
     uint8_t valid;
     uint16_t seqNum;
     uint16_t pduSize;
-    uint8_t *pdu;
+    uint8_t pdu[1407];
 };
 
 struct window{
-    struct frame *frames;
+    struct frame* frames;
     uint16_t availableSize;
     uint16_t usedSize;
     uint16_t lower;
@@ -35,11 +36,61 @@ struct window* init( int winSize ){
     return newWin;
 };
 
-uint8_t enqueue(struct window* win){
+void printMetaData(struct window* win){
+    printf("Server Window - Window Size: %d, lower: %d, Upper: %d, Current: %d\n",
+           win->availableSize, win->lower, win->upper, win->current);
+}
+
+void printWindow(struct window* win){
+    printf("Window size is: %d\n", win->availableSize);
+    for(int i = 0; i<win->availableSize; i++){
+        struct frame* currentFrame = &win->frames[i];
+        printf("\t%d ", i);
+        if(currentFrame->valid)
+            printf("sequenceNumber: %d pduSize: %d\n", currentFrame->seqNum, currentFrame->pduSize);
+        else
+            printf("not valid\n");
+    }
+}
+
+
+uint8_t enqueue(struct window* win, uint16_t seqNum, uint16_t pduSize, uint8_t *pdu){
     if(win->usedSize > win->availableSize){
         return 0;
     }
 
-    
+    struct frame* newFrame = &win->frames[seqNum % win->availableSize];
+    newFrame->valid     = 1;
+    newFrame->seqNum    = seqNum;
+    memcpy(&newFrame->pdu[0], &pdu[0], pduSize);
+
+    (win->usedSize)++;
+
     return 1;
 }
+
+uint8_t processRR(struct window* win, uint16_t seqNum){
+    if(seqNum > win->current){
+        return 0;
+    }
+    uint16_t cursor = win->lower;
+    while(cursor < seqNum){
+        (win->upper)++;
+        (win->lower)++;
+        struct frame* oldFrame = &win->frames[seqNum % win->availableSize];
+        oldFrame->valid = 0;
+        (win->usedSize)--;
+        cursor++;
+    }
+    return 1;
+}
+
+uint16_t getPDU(uint8_t* dest, struct window* win, uint16_t seqNum){
+    uint16_t cursor = win->lower;
+    while(cursor < seqNum){
+        cursor++;
+    }
+}
+
+
+
---
+++ window.h
@@ -0,0 +1,29 @@
+//
+// Created by tyler on 5/18/2022.
+//
+
+#ifndef UDP_WINDOW_H
+#define UDP_WINDOW_H
+
+#include <stdint-gcc.h>
+#include <malloc.h>
+#include <string.h>
+
+struct frame;
+
+struct window;
+
+struct window* init( int winSize );
+
+void printMetaData(struct window* win);
+
+void printWindow(struct window* win);
+
+
+uint8_t enqueue(struct window* win, uint16_t seqNum, uint16_t pduSize, uint8_t *pdu);
+
+uint8_t processRR(struct window* win, uint16_t seqNum);
+
+uint16_t getPDU(uint8_t* dest, struct window* win, uint16_t seqNum);
+
+#endif //UDP_WINDOW_H
